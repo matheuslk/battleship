46f3743c9db0a050af5e4f8fc51889b2ed40862b
matheuslk <matheus.augustolk@gmail.com>
2022-06-12 21:01:47 -0300

    batalha naval finalizada

--- batalha-naval.py
+++ batalha-naval.py
@@ -7,24 +7,18 @@ colunas_matriz = ['0', '1', '2', '3', '4', '5', '6',
                   '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19']
 for linha in range(len(matriz_valores)):
   matriz_valores[linha] = ['▮'] * 20
-# quantidadePortaAviao = 3
-# tamanhoPortaAviao = 4
-# quantidadeCruzador = 4
-# tamanhoCruzador = 3
-# quantidadeFragata = 5
-# tamanhoFragata = 2
 PORTA_AVIAO = '2'
 CRUZADOR = '1'
 FRAGATA = '0'
-quantidade_porta_aviao = 1
+quantidade_porta_aviao = 3
 tamanho_porta_aviao = 4
 posicoes_porta_aviao = []
 
-quantidade_cruzador = 1
+quantidade_cruzador = 4
 tamanho_cruzador = 3
 posicoes_cruzador = []
 
-quantidade_fragata = 1
+quantidade_fragata = 5
 tamanho_fragata = 2
 posicoes_fragata = []
 
@@ -34,19 +28,7 @@ posicoes_fragata = []
 pontuacoes = [10, 20, 30]
 
 
-def calcularPontuacaoFinal():
-  pontuacao = 0
-  for index_pontuacao in range(len(pontuacoes)):
-    if(index_pontuacao == 0):
-      pontuacao += quantidade_fragata * pontuacoes[index_pontuacao]
-    elif(index_pontuacao == 1):
-      pontuacao += quantidade_cruzador * pontuacoes[index_pontuacao]
-    else:
-      pontuacao += quantidade_porta_aviao * pontuacoes[index_pontuacao]
-  return pontuacao
-
-
-def calcularPosicoesEmbarcacao(tipo_embarcacao):
+def calcular_posicoes_embarcacao(tipo_embarcacao):
   quantidade_embarcacao = 0
   tamanho_embarcacao = 0
   if(tipo_embarcacao == 0):
@@ -69,18 +51,42 @@ def calcularPosicoesEmbarcacao(tipo_embarcacao):
 
 for index_tipo_embarcacao in range(3):
   if(index_tipo_embarcacao == 0):
-    posicoes_fragata = calcularPosicoesEmbarcacao(index_tipo_embarcacao)
+    posicoes_fragata = calcular_posicoes_embarcacao(index_tipo_embarcacao)
   elif index_tipo_embarcacao == 1:
-    posicoes_cruzador = calcularPosicoesEmbarcacao(index_tipo_embarcacao)
+    posicoes_cruzador = calcular_posicoes_embarcacao(index_tipo_embarcacao)
   else:
-    posicoes_porta_aviao = calcularPosicoesEmbarcacao(index_tipo_embarcacao)
+    posicoes_porta_aviao = calcular_posicoes_embarcacao(index_tipo_embarcacao)
 
 
-def atualizarPosicoesEmbarcacao(linha, coluna, tipo_embarcacao, index_embarcacao):
+def atualizar_posicoes_embarcacao(linha, coluna, tipo_embarcacao, index_embarcacao):
   todas_posicoes = [posicoes_porta_aviao, posicoes_cruzador, posicoes_fragata]
   for index_posicao in range(len(todas_posicoes[tipo_embarcacao][index_embarcacao])):
     todas_posicoes[tipo_embarcacao][index_embarcacao][index_posicao] = [
         linha, coluna+index_posicao]
+
+
+def desenhar_tabuleiro(matriz):
+  for linha in range(len(matriz)):
+    if(linha == 0):
+      for index_numero_coluna in range(20):
+        if(index_numero_coluna == 0):
+          print(f'  {index_numero_coluna}', end='')
+        elif(index_numero_coluna == 19):
+          print(f' {index_numero_coluna}')
+        elif(index_numero_coluna > 0 and index_numero_coluna < 10):
+          print(f'  {index_numero_coluna}', end='')
+        elif(index_numero_coluna == 10):
+          print(f'  {index_numero_coluna}', end='')
+        elif(index_numero_coluna > 10):
+          print(f' {index_numero_coluna}', end='')
+    for coluna in range(20):
+      if(coluna == 0):
+        print(
+            f'{linhas_matriz[linha]} {matriz[linha][coluna]}  ', end='')
+      elif (coluna == 19):
+        print(f'{matriz[linha][coluna]}')
+      else:
+        print(f'{matriz[linha][coluna]}  ', end='')
 
 # CADASTRO DE EMBARCAÇÕES
 
@@ -108,18 +114,7 @@ while numero_cadastro_atual < total_tipos_embarcacao:
     conteudo_embarcacao = FRAGATA
   while quantidade_embarcacao_cadastrada < total_embarcacoes:
     os.system('cls||clear')
-    for linha in range(len(matriz_valores)):
-      if(linha == 0):
-        print(
-            '\n  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19')
-      for coluna in range(20):
-        if(coluna == 0):
-          print(
-              f'{linhas_matriz[linha]} {matriz_valores[linha][coluna]}  ', end='')
-        elif (coluna == 19):
-          print(f'{matriz_valores[linha][coluna]}')
-        else:
-          print(f'{matriz_valores[linha][coluna]}  ', end='')
+    desenhar_tabuleiro(matriz_valores)
     if(numero_cadastro_atual == 0):
       print(
           f'\n> Cadastre {total_embarcacoes-quantidade_embarcacao_cadastrada} porta-aviões')
@@ -143,7 +138,7 @@ while numero_cadastro_atual < total_tipos_embarcacao:
         for index_posicao_ocupada in range(tamanho_embarcacao):
           matriz_valores[linha_embarcacao][coluna_embarcacao +
                                            index_posicao_ocupada] = conteudo_embarcacao
-        atualizarPosicoesEmbarcacao(
+        atualizar_posicoes_embarcacao(
             linha_embarcacao, coluna_embarcacao, numero_cadastro_atual, quantidade_embarcacao_cadastrada)
         quantidade_embarcacao_cadastrada += 1
       else:
@@ -166,20 +161,27 @@ def ataque(linha, coluna):
     return
   tipo_embarcacao_atacada = int(matriz_valores[linha][coluna])
   todas_posicoes = [posicoes_fragata, posicoes_cruzador, posicoes_porta_aviao]
-  # print(f'\n> Tipo embarcacao {tipo_embarcacao_atacada}')
-  # print(f'\n> INDEX POSICOES EMBARCACAO {todas_posicoes[tipo_embarcacao_atacada]}')
   for index_posicoes_embarcacao in range(len(todas_posicoes[tipo_embarcacao_atacada])):
     for index_embarcacao in range(len(todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao])):
-      # print(f'TESTE {todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao][index_embarcacao]}')
-      # print(f'LINHA ATACADA {linha}')
-      # print(f'COLUNA ATACADA {coluna}')
       if(todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao][index_embarcacao] and
          todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao][index_embarcacao][0] == linha and
          todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao][index_embarcacao][1] == coluna):
         todas_posicoes[tipo_embarcacao_atacada][index_posicoes_embarcacao][index_embarcacao] = NULL
 
 
-def calcularPontuacao():
+def calcular_pontuacao_final():
+  pontuacao = 0
+  for index_pontuacao in range(len(pontuacoes)):
+    if(index_pontuacao == 0):
+      pontuacao += quantidade_fragata * pontuacoes[index_pontuacao]
+    elif(index_pontuacao == 1):
+      pontuacao += quantidade_cruzador * pontuacoes[index_pontuacao]
+    else:
+      pontuacao += quantidade_porta_aviao * pontuacoes[index_pontuacao]
+  return pontuacao
+
+
+def calcular_pontuacao_total():
   pontuacao = 0
   todas_posicoes = [posicoes_fragata, posicoes_cruzador, posicoes_porta_aviao]
   for tipo_embarcacao in range(3):
@@ -199,27 +201,14 @@ matriz_jogo = [0] * 20
 for linha in range(len(matriz_jogo)):
   matriz_jogo[linha] = ['▮'] * 20
 pontuacao_total = 0
-pontuacao_final = calcularPontuacaoFinal()
-fim_jogo = False
-while not fim_jogo:
+pontuacao_final = calcular_pontuacao_final()
+while True:
   os.system('cls||clear')
-  for linha in range(len(matriz_jogo)):
-    if(linha == 0):
-      print(
-          '\n  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19')
-    for coluna in range(20):
-      if(coluna == 0):
-        print(
-            f'{linhas_matriz[linha]} {matriz_jogo[linha][coluna]}  ', end='')
-      elif (coluna == 19):
-        print(f'{matriz_jogo[linha][coluna]}')
-      else:
-        print(f'{matriz_jogo[linha][coluna]}  ', end='')
+  desenhar_tabuleiro(matriz_jogo)
+  print(f'\n>Pontuacao Total: {pontuacao_total}')
   if(pontuacao_final == pontuacao_total):
-    fim_jogo = True
     input('\nParabéns, você venceu ! Digite qualquer tecla para finalizar o jogo...')
     break
-  print(f'\n>Pontuacao Total: {pontuacao_total}')
   linha_ataque = input('\n>Digite a linha: ').upper()
   coluna_ataque = input('\n>Digite a coluna: ')
   if linha_ataque in linhas_matriz and coluna_ataque in colunas_matriz:
@@ -230,7 +219,7 @@ while not fim_jogo:
       posicao_disponivel = False
     if posicao_disponivel:
       ataque(linha_ataque, coluna_ataque)
-      pontuacao_total = calcularPontuacao()
+      pontuacao_total = calcular_pontuacao_total()
     else:
       input('\n> Posição inválida, digite qualquer tecla para continuar...')
   else:
